d4aba862257a4238be03106d2cf548cff19ac990
Moses <mutisyamoses722@gmail.com>
2024-06-12 08:53:14 +0300

    validating  account balance

--- lib/models/accounts.py
+++ lib/models/accounts.py
@@ -1,12 +1,12 @@
 from .config import get_db_connection
 from .setup import create_tables, drop_tables
-from .data_validation import validate_account_number
+from .data_validation import validate_account_number,validate_account_balance
 
 class Account:
     def __init__(self, account_number, account_balance, account_type, date_opened, id=None):
         self.id = id
         self.account_number = validate_account_number(account_number)
-        self.account_balance = account_balance
+        self.account_balance = validate_account_balance(account_balance)
         self.account_type= account_type
         self.date_opened = date_opened
 
--- lib/models/data_validation.py
+++ lib/models/data_validation.py
@@ -2,3 +2,12 @@ def validate_account_number(account_number):
     if len(account_number) != 10 or not account_number.isdigit():
         raise ValueError("Account number must be exactly 10 digits long.")
     return account_number
+
+def validate_account_balance(account_balance):
+    try:
+        balance = float(account_balance)
+        if balance < 0:
+            raise ValueError("Account balance cannot be negative.")
+    except ValueError:
+        raise ValueError("Account balance must be a valid number.")
+    return balance
